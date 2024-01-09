fb9520f2a8729d70d1369853948ecd75db12acb5
Seba <sebastiantorresola3@gmail.com>
2024-01-09 12:06:49 -0800

    Added more descriptive button, fixed styling, and made button only appear on full courses

--- extension/src/utils.js
+++ extension/src/utils.js
@@ -1,15 +1,39 @@
 export const insertButtons = (courseList) => {
-    courseList.childNodes.forEach((child, idx) => {
+    courseList.childNodes.forEach((courseRow, idx) => {
         // Returns if course has already been modified (prevents infinite loop)
-        if (child.classList.contains('insertedStatus')) {
+        if (courseRow.classList.contains('insertedStatus')) {
             return;
         }
 
-        child.classList.add("insertedStatus");
-        if (idx > 0) {
-            const waitlistButton = document.createElement("button");
-            waitlistButton.innerText = "Testing!";
-            child.appendChild(waitlistButton);
+        if (courseRow.classList.contains("course-section--viewing")) {
+            courseRow.style.pointerEvents = "auto";
+        }
+
+        courseRow.classList.add("insertedStatus");
+        if (idx > 0 && courseFull(courseRow)) {
+            const waitlistButton = createWaitlistButton();
+            courseRow.appendChild(waitlistButton);
         }
     });
+}
+
+const courseFull = (courseRow) => {
+    const enrollCap = courseRow.childNodes[5].childNodes[1].textContent;
+    const enrollAmount = courseRow.childNodes[6].childNodes[1].textContent;
+    return enrollAmount === enrollCap;
+}
+
+const createWaitlistButton = () => {
+    const button = document.createElement('button');
+    button.style.width = "125px";
+    button.style.height = "55px";
+    button.style.padding = "0.5rem"
+    button.innerText = "Add to Waitlist Notifier";
+    button.onclick = sendWaitlistRequest;
+
+    return button;
+}
+
+const sendWaitlistRequest = (courseRow) => {
+    console.log("Updated user's waitlist preferences.")
 }
